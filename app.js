e8aef9fc836b17a6774d73a5d807a0fe3daa7d08
geethika105 <geethikaudatha82@gmail.com>
2026-02-04 11:52:43 +0530

    playing with the routes

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express=require('express')
 const app=express()
 //This will only handle get call to /user
-app.get("/user",(req,res)=>{
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
     res.send({"firstname":"geethika","lastname":"yadav"})
 })
 //This will only handle post call to /user
-app.post("/user",(req,res)=>{
-    res.send('data successfully saved to the database')
-})
+// app.post("/user",(req,res)=>{
+//     res.send('data successfully saved to the database')
+// })
 //This will only handle delete call to /user
-app.delete("/user",(req,res)=>{
-    res.send('data deleted successfully')
-})
+// app.delete("/user",(req,res)=>{
+//     res.send('data deleted successfully')
+// })
 //this will match all http method api calls to /test
-app.use("/",(req,res)=>{
-    res.send("hello")
-})
+// app.use("/",(req,res)=>{
+//     res.send("hello")
+// })
 // app.use("/hello",(req,res)=>{
 //     res.send("hello geethika")
 // })
