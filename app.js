63ca74415b1eefb730803d466b24a0023c86868d
geethika105 <geethikaudatha82@gmail.com>
2026-02-04 16:31:41 +0530

    wildcard-error-handling

--- app.js
+++ app.js
@@ -1,45 +1,19 @@
 const express=require('express')
 const app=express()
-const {adminAuth}=require('./middlewares/auth')
-// dynamic routing
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params)
-    res.send({"firstname":"geethika","lastname":"yadav"})
-})
-// This will only handle post call to /user
-app.post("/user",(req,res)=>{
-    res.send('data successfully saved to the database')
-})
-// This will only handle delete call to /user
-app.delete("/user",(req,res)=>{
-    res.send('data deleted successfully')
-})
-app.use("/",(req,res)=>{
-    res.send("hello")
-})
-app.use("/hello",(req,res)=>{
-    res.send("hello geethika")
-})
-app.use("/",(req,res)=>{
-    res.send("hello")
-})
 
-// GET /users =>middleware chain => request handler
-app.use("/",(req,res,next)=>{
-    // res.send("handling / Route")
-    next()
+app.get('/getUserData',(req,res)=>{
+    try{
+        //logic of DB call and get user data
+    throw new Error("njjj")
+     res.send("user data sent")
+    }
+    catch(err)
+    {
+    res.status(500).send("some error contact support team")
+    }
 })
-app.get("/user",(req,res,next)=>{
-    res.send("Handling /user route")
-    next()
-})
-//working of middlewares
-app.use("/admin",adminAuth)
-app.get('/admin/getAllData',(req,res)=>{
-    res.send("All data sent")
-})
-app.get('/admin/DeleteAllData',(req,res)=>{
-    res.send("Deleted a user")
+app.use('/',(err,req,res,next)=>{
+    res.status(500).send("something went wrong")
 })
 app.listen(3000,()=>{
     console.log("server is running successfully on port 3000")
