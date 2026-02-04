6774667ca8f47ac45dba2fbb62e73de7d8f8c1d1
geethika105 <geethikaudatha82@gmail.com>
2026-02-04 10:52:23 +0530

    exploring get,post,delete

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express=require('express')
 const app=express()
-app.use("/test",(req,res)=>{
-    res.send("hello from server")
+//This will only handle get call to /user
+app.get("/user",(req,res)=>{
+    res.send({"firstname":"geethika","lastname":"yadav"})
 })
-app.use("/hello",(req,res)=>{
-    res.send("hello geethika")
+//This will only handle post call to /user
+app.post("/user",(req,res)=>{
+    res.send('data successfully saved to the database')
 })
+//This will only handle delete call to /user
+app.delete("/user",(req,res)=>{
+    res.send('data deleted successfully')
+})
+//this will match all http method api calls to /test
+app.use("/",(req,res)=>{
+    res.send("hello")
+})
+// app.use("/hello",(req,res)=>{
+//     res.send("hello geethika")
+// })
+// app.use("/",(req,res)=>{
+//     res.send("hello")
+// })
 app.listen(3000,()=>{
     console.log("server is running successfully on port 3000")
 })
