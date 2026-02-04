011e08fbe918da2cca48b4bbe0fba8f9e56cb9df
geethika105 <geethikaudatha82@gmail.com>
2026-02-04 15:43:50 +0530

    http methods

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 const express=require('express')
 const app=express()
 const {adminAuth}=require('./middlewares/auth')
+// dynamic routing
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
+    res.send({"firstname":"geethika","lastname":"yadav"})
+})
+// This will only handle post call to /user
+app.post("/user",(req,res)=>{
+    res.send('data successfully saved to the database')
+})
+// This will only handle delete call to /user
+app.delete("/user",(req,res)=>{
+    res.send('data deleted successfully')
+})
+app.use("/",(req,res)=>{
+    res.send("hello")
+})
+app.use("/hello",(req,res)=>{
+    res.send("hello geethika")
+})
+app.use("/",(req,res)=>{
+    res.send("hello")
+})
+
+// GET /users =>middleware chain => request handler
+app.use("/",(req,res,next)=>{
+    // res.send("handling / Route")
+    next()
+})
+app.get("/user",(req,res,next)=>{
+    res.send("Handling /user route")
+    next()
+})
 //working of middlewares
 app.use("/admin",adminAuth)
 app.get('/admin/getAllData',(req,res)=>{
